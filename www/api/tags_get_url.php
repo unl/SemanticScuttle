aaf9189a867ba3dc320bd6d9c9d0ec25d0903fe8
Michael Fairchild <mfairchild365@gmail.com>
2012-10-25 13:55:44 -0500

    Add an api for getting public tags for a given url.

---
+++ www/api/tags_get_url.php
@@ -0,0 +1,46 @@
+<?php
+// Allows for an API call to get all public tags for a given url.
+
+//Send as json
+$httpContentType = 'application/json';
+
+// Do NOT force auth.
+require_once '../www-header.php';
+
+//Get the url to query
+$url = "";
+if (isset($_GET['url'])) {
+    $url = md5(urldecode($_GET['url']));
+}
+
+/* Service creation: only useful services are created */
+$bookmarkService = SemanticScuttle_Service_Factory::get('Bookmark');
+$b2tservice      = SemanticScuttle_Service_Factory::get('Bookmark2Tag');
+
+//Create an array of tags
+$tags = array();
+
+//Fail early if no url was given.  (if empty url, tags may still be returned?).
+if (empty($url)) {
+    echo json_encode($tags);
+    exit();
+}
+
+//Get all bookmarks from everyone for the given url.
+$bookmarks = $bookmarkService->getBookmarks(0, null, null, null, null, null, null, null, null, $url);
+
+$tags = array();
+
+//look though bookmarks to get tags...
+foreach ($bookmarks['bookmarks'] as $bookmark) {
+    //Do not get tags for this bookmark if it is not public (public = 0, limited=1, private=2)
+    if ($bookmark['bStatus'] != 0) {
+        continue;
+    }
+    
+    //actually get the tags
+    $tags += $b2tservice->getTagsForBookmark($bookmark['bId'], true);
+}
+
+//Echo a json encoded array of unique tags.
+echo json_encode(array_unique($tags));
